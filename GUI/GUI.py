c0c3199a4d02f8a4cebfc5eba278a6ba0e139c0b
Eyeo <tsrobertson2@bsu.edu>
2025-09-02 17:48:24 -0400

    Formatted correctly for the most part GUI

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -137,54 +137,54 @@ def buttonPressToggle():
 window =  tk.Tk()
 window.geometry("400x400")
 window['background'] = "black"
-field=tk.Text(window, height=2, width=21, font=("Times New Roman", 20))
-field.grid(row=1, column = 1, columnspan=5)
+field=tk.Text(window, height=2, width=29, font=("Times New Roman", 20))
+field.grid(row=0, column = 0, columnspan=5)
 
 
 #Number Buttons
 
 btn0 = tk.Button(window, text="0", command=lambda: addToField(0), width = 5, font = ("Times New Roman", 14))
-btn0.grid(row=2, column = 1, pady = 10)
+btn0.grid(row=1, column = 0, pady = 10)
 
 btn1 = tk.Button(window, text="1", command=lambda: addToField(1), width = 5, font = ("Times New Roman", 14))
-btn1.grid(row=2, column = 2, pady = 10)
+btn1.grid(row=1, column = 1, pady = 10)
 
 btn2 = tk.Button(window, text="2", command=lambda: addToField(2), width = 5, font = ("Times New Roman", 14))
-btn2.grid(row=2, column = 3, pady = 10)
+btn2.grid(row=1, column = 2, pady = 10)
 
 btn3 = tk.Button(window, text="3", command=lambda: addToField(3), width = 5, font = ("Times New Roman", 14))
-btn3.grid(row=2, column = 4, pady = 10)
+btn3.grid(row=1, column = 3, pady = 10)
 
 btn4 = tk.Button(window, text="4", command=lambda: addToField(4), width = 5, font = ("Times New Roman", 14))
-btn4.grid(row=2, column = 5, pady = 10)
+btn4.grid(row=1, column = 4, pady = 10)
 
 #Operations Buttons
 
 btnDiv = tk.Button(window, text="/", command=lambda: buttonPressDiv(), width = 5, font = ("Times New Roman", 14))
-btnDiv.grid(row=3, column = 4, pady = 10)
+btnDiv.grid(row=2, column = 3, pady = 10)
 
 btnMult = tk.Button(window, text="*", command=lambda: buttonPressMult(), width = 5, font = ("Times New Roman", 14))
-btnMult.grid(row=3, column = 3, pady = 10)
+btnMult.grid(row=2, column = 2, pady = 10)
 
 btnAdd = tk.Button(window, text="+", command=lambda: buttonPressAdd(), width = 5, font = ("Times New Roman", 14))
-btnAdd.grid(row=3, column = 1, pady = 10)
+btnAdd.grid(row=2, column = 0, pady = 10)
 
 btnSub = tk.Button(window, text="-", command=lambda: buttonPressSub(), width = 5, font = ("Times New Roman", 14))
-btnSub.grid(row=3, column = 2, pady = 10)
+btnSub.grid(row=2, column = 1, pady = 10)
 
 btnClr = tk.Button(window, text="Clear", command=lambda: clear(), width = 5, font = ("Times New Roman", 14))
-btnClr.grid(row=3, column = 5, pady = 10)
+btnClr.grid(row=2, column = 4, pady = 10)
 
 btnTog = tk.Button(window, text="Tog", command=lambda: buttonPressToggle() , width = 5, font = ("Times New Roman", 14))
-btnTog.grid(row=4, column = 3, pady = 10)
+btnTog.grid(row=3, column = 2, pady = 10)
 
 btnSqrt = tk.Button(window, text="sqrt", command=lambda: buttonPressSqrt(), width = 5, font = ("Times New Roman", 14))
-btnSqrt.grid(row=4, column = 4, pady = 10)
+btnSqrt.grid(row=3, column = 3, pady = 10)
 
 btnSqr = tk.Button(window, text="sqr", command=lambda: buttonPressSqu() , width = 5, font = ("Times New Roman", 14))
-btnSqr.grid(row = 4, column = 5, pady = 10)
+btnSqr.grid(row = 3, column = 4, pady = 10)
 
-btnEqu = tk.Button(window, text="=", command=lambda: calculate(), width = 20, font = ("Times New Roman", 14))
-btnEqu.grid(row=4, column = 1, columnspan=2, pady = 10)
+btnEqu = tk.Button(window, text="=", command=lambda: calculate(), width = 10, font = ("Times New Roman", 14))
+btnEqu.grid(row=3, column = 0, columnspan=2, pady = 10)
 
 window.mainloop()
