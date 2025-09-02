99619965040522c756bef0e7360de1469ad8f4d1
Eyeo <tsrobertson2@bsu.edu>
2025-09-02 15:53:33 -0400

    Update GUI
    Made buttons do the function they are supposed to (hopefully) and regridded the gui

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+from ..logic import logic
 
 fieldText = ""
+
+quin = False
 
 def addToField(sth):
     global fieldText
@@ -42,38 +45,38 @@ btn2 = tk.Button(window, text="2", command=lambda: addToField(2), width = 5, fon
 btn2.grid(row=2, column = 3, pady = 10)
 
 btn3 = tk.Button(window, text="3", command=lambda: addToField(3), width = 5, font = ("Times New Roman", 14))
-btn3.grid(row=3, column = 1, pady = 10)
+btn3.grid(row=2, column = 4, pady = 10)
 
 btn4 = tk.Button(window, text="4", command=lambda: addToField(4), width = 5, font = ("Times New Roman", 14))
-btn4.grid(row=3, column = 2, pady = 10)
-
-btn5 = tk.Button(window, text="5", command=lambda: addToField(5), width = 5, font = ("Times New Roman", 14))
-btn5.grid(row=3, column = 3, pady = 10)
+btn4.grid(row=2, column = 5, pady = 10)
 
 #Operations Buttons
 
 btnDiv = tk.Button(window, text="/", command=lambda: addToField("/"), width = 5, font = ("Times New Roman", 14))
-btnDiv.grid(row=2, column = 4, pady = 10)
+btnDiv.grid(row=3, column = 4, pady = 10)
 
 btnMult = tk.Button(window, text="*", command=lambda: addToField("*"), width = 5, font = ("Times New Roman", 14))
-btnMult.grid(row=3, column = 4, pady = 10)
+btnMult.grid(row=3, column = 3, pady = 10)
 
 btnAdd = tk.Button(window, text="+", command=lambda: addToField("+"), width = 5, font = ("Times New Roman", 14))
-btnAdd.grid(row=4, column = 4, pady = 10)
+btnAdd.grid(row=3, column = 1, pady = 10)
 
 btnSub = tk.Button(window, text="-", command=lambda: addToField("-"), width = 5, font = ("Times New Roman", 14))
-btnSub.grid(row=5, column = 4, pady = 10)
+btnSub.grid(row=3, column = 2, pady = 10)
 
 btnClr = tk.Button(window, text="Clear", command=lambda: clear(), width = 5, font = ("Times New Roman", 14))
-btnClr.grid(row=4, column = 3, pady = 10)
+btnClr.grid(row=3, column = 5, pady = 10)
 
-btnOpenPar = tk.Button(window, text="(", command=lambda: addToField("("), width = 5, font = ("Times New Roman", 14))
-btnOpenPar.grid(row=4, column = 1, pady = 10)
+btnTog = tk.Button(window, text="Tog", command=lambda: logic.convert_to_decimal(int(fieldText)) if quin is True else logic.convert_to_quinary(int(fieldText)), width = 5, font = ("Times New Roman", 14))
+btnTog.grid(row=4, column = 1, pady = 10)
 
-btnClosePar = tk.Button(window, text=")", command=lambda: addToField(")"), width = 5, font = ("Times New Roman", 14))
-btnClosePar.grid(row=4, column = 2, pady = 10)
+btnSqrt = tk.Button(window, text="sqrt", command=lambda: logic.sqrt(int(fieldText)), width = 5, font = ("Times New Roman", 14))
+btnSqrt.grid(row=4, column = 2, pady = 10)
+
+btnSqr = tk.Button(window, text="sqr", command=lambda: logic.sqr(int(fieldText)) , width = 5, font = ("Times New Roman", 14))
+btnSqr.grid(row = 4, column = 3)
 
 btnEqu = tk.Button(window, text="=", command=lambda: calculate(), width = 20, font = ("Times New Roman", 14))
-btnEqu.grid(row=5, column = 1, columnspan=3, pady = 10)
+btnEqu.grid(row=4, column = 4, columnspan=2, pady = 10)
 
 window.mainloop()
